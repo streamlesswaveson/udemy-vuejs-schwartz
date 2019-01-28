11efa8bce928bf12414d70b29005b7508c6000bb
streamlesswaveson <streamlesswaveson@gmail.com>
2019-01-28 05:39:50 -0800

    section 7, assignment 6 - components and scoped css

---
+++ 07-introduction-to-components/01-exercise/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+
+        <exercise-header></exercise-header>
+
+        <hr>
+        <exercise-dashboard></exercise-dashboard>
+        <hr>
+        <exercise-footer></exercise-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import ServerDashboard from './ServerDashboard.vue';
+    export default {
+        components: {
+            'exercise-header': Header,
+            'exercise-footer': Footer,
+            'exercise-dashboard': ServerDashboard
+        }
+    }
+</script>
+
+<style>
+
+</style>
