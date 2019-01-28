4c74eecba3dafabc34a6a62a94965c5530df4ebd
streamlesswaveson <streamlesswaveson@gmail.com>
2019-01-28 04:53:48 -0800

    7-96 - global and local components

---
+++ 07-introduction-to-components/096-local-and-global-components/src/ServerStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Server status is {{status}}</p>
+    <hr>
+    <button @click="changeStatus">Change</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        status: 'Critical'
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.status = 'Normal';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
