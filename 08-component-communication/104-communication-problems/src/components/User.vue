ca6fb3079b71407d6b9f640a754a37c902f32f3b
streamlesswaveson <streamlesswaveson@gmail.com>
2019-01-29 05:15:14 -0800

    section 8-105 - using v-bind to pass properties to child components

--- 08-component-communication/104-communication-problems/src/components/User.vue
+++ 08-component-communication/104-communication-problems/src/components/User.vue
@@ -2,10 +2,12 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeMyName">Change My Name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <!-- use v-bind:name or just :name -->
+                <app-user-detail v-bind:name="name"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +21,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function(){
+            return {
+                name: "streamless"
+            };
+        },
+        methods: {
+            changeMyName() {
+                this.name = 'Josephine';
+            }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- 08-component-communication/104-communication-problems/src/components/UserDetail.vue
+++ 08-component-communication/104-communication-problems/src/components/UserDetail.vue
@@ -2,10 +2,16 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: {{name}}</p>
     </div>
 </template>
 
 <script>
+    export default {
+        // props - properties that can be set from outside
+        // these must match the properties used in the template
+        props: ['name']
+    }
 </script>
 
 <style scoped>
