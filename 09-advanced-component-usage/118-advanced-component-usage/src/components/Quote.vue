d44e1d50ad7c4fdb10123c4ebcc34831ec75209e
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-01 04:38:15 -0800

    9-118 - add quote component

---
+++ 09-advanced-component-usage/118-advanced-component-usage/src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Some quote!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Quote"
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: auto;
+        text-align: center;
+
+    }
+</style>
