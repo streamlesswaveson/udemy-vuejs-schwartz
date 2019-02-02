d270852d48930f236ad624dbfa48ee89ef4bf114
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-02 12:16:16 -0800

    9-127 - dynamic component default lifecycle

--- 09-advanced-component-usage/118-advanced-component-usage/src/components/AppNew.vue
+++ 09-advanced-component-usage/118-advanced-component-usage/src/components/AppNew.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{counter}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "AppNew"
+        name: "AppNew",
+        data : function () {
+            return {
+                counter: 0
+            };
+        },
+        destroyed() {
+            console.log('AppNew destroyed');
+        }
     }
 </script>
 
