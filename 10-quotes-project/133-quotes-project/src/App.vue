ac898374aa5fbaae963ca637cb449893517e35a6
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-03 07:06:43 -0800

    10-138 - add new quote

--- 10-quotes-project/133-quotes-project/src/App.vue
+++ 10-quotes-project/133-quotes-project/src/App.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import NewQuote from './components/NewQuote.vue';
     import QuoteGrid from './components/QuoteGrid.vue';
     export default {
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        },
+        methods: {
+          newQuote(quote) {
+              if (quote) {
+                  console.log('hey new quote', quote);
+                  this.quotes.push(quote);
+              }
+          }
         },
         data: function () {
 
---
+++ 10-quotes-project/133-quotes-project/src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <!-- click.prevent - prevents default behavior, which is sending the form to the server -->
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+              quote: ''
+            };
+        },
+        methods: {
+            createNew(){
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        },
+        name: "NewQuote"
+    }
+</script>
+
+<style scoped>
+
+</style>
