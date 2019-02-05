fdf21b63979fbdace74fd2e4549ee953181c3f64
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-05 05:15:41 -0800

    11-154 - submitting a form

--- 11-user-input-with-forms/145-user-input-with-forms/src/App.vue
+++ 11-user-input-with-forms/145-user-input-with-forms/src/App.vue
@@ -108,13 +108,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                    @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -145,8 +146,14 @@
         components: {
             appSwitch: Switch
         },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        },
         data: function () {
             return {
+                isSubmitted: false,
                 userData: {
                     email: '',
                     password: '',
