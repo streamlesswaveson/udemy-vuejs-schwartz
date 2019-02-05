17599abbabde5ca277fc4ad2d349f67835b06a54
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-05 04:54:14 -0800

    11-151 - dropdowns with option and select

--- 11-user-input-with-forms/145-user-input-with-forms/src/App.vue
+++ 11-user-input-with-forms/145-user-input-with-forms/src/App.vue
@@ -93,8 +93,9 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                    v-model="selectedPriority">
+                        <option v-for="priority in priorities" :selected="priority === 'Medium'">{{priority}}</option>
                     </select>
                 </div>
             </div>
@@ -124,7 +125,7 @@
                             <li v-for="item in sendMail">{{item}}</li>
                         </ul>
                         <p>Gender: {{gender}}</p>
-                        <p>Priority:</p>
+                        <p>Priority: {{selectedPriority}}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -144,7 +145,9 @@
                 },
                 message: 'default message',
                 sendMail: [],
-                gender: 'Male'
+                gender: 'Male',
+                selectedPriority: 'High',
+                priorities: ['High','Medium','Low']
             };
 
         }
