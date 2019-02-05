7f5a0755a61d30304e5a15b3c7d7346857dfedda
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-05 04:45:47 -0800

    11-149 - using checkboxes, saving data in arrays

--- 11-user-input-with-forms/145-user-input-with-forms/src/App.vue
+++ 11-user-input-with-forms/145-user-input-with-forms/src/App.vue
@@ -56,13 +56,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                            v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                            v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -117,7 +119,7 @@
                         <p style="white-space: pre">Message: {{message}}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{item}}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -138,7 +140,8 @@
                     password: '',
                     age: 27
                 },
-                message: 'default message'
+                message: 'default message',
+                sendMail: []
             };
 
         }
