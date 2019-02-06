bfd86f96f87c824f19a879f3bdaa105d2a4abe83
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-06 05:02:01 -0800

    12-165 - registering a local directive

--- 12-using-creating-directives/158-directives/src/App.vue
+++ 12-using-creating-directives/158-directives/src/App.vue
@@ -22,11 +22,37 @@
 
             </div>
         </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Local Directives</h1>
+                <p v-local-highlight:background.delayed="'purple'">Local directive</p>
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+
+                    setTimeout( ()=> {
+                        el.style.color = 'white';
+                        if (binding.arg === 'background') {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
+            }
+        }
     }
 </script>
 
