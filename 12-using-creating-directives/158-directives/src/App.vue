ddb6ecf8538eceaa76141c14c76a67b04fc2f1dc
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-06 04:44:33 -0800

    12-160 - simple global highlight directive v-highlight

--- 12-using-creating-directives/158-directives/src/App.vue
+++ 12-using-creating-directives/158-directives/src/App.vue
@@ -2,7 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
+                <h1>Built in Directives</h1>
+                <p v-text="'some text'"></p>
+                <p v-html="'<strong>some strong text</strong>'"></p>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight>Highlight this</p>
 
             </div>
         </div>
