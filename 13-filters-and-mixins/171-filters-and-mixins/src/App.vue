acc8a5817eb72a1ed631ad7e5abe0d4c7b0c05f7
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-09 07:10:57 -0800

    13-172 - global filters, chaining filters

--- 13-filters-and-mixins/171-filters-and-mixins/src/App.vue
+++ 13-filters-and-mixins/171-filters-and-mixins/src/App.vue
@@ -3,7 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{text | toUpperCase}}</p>
+                <!-- chained filters -->
+                <p>{{text | toUpperCase | toLowerCase}}</p>
 
             </div>
         </div>
@@ -14,7 +15,7 @@
     export default {
         data() {
             return {
-                text: 'Hello there'
+                text: 'Hello There'
             }
         },
         filters: {
--- 13-filters-and-mixins/171-filters-and-mixins/src/main.js
+++ 13-filters-and-mixins/171-filters-and-mixins/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// global filter
+Vue.filter('toLowerCase', function (value) {
+  return value.toLowerCase();
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
