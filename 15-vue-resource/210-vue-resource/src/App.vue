d9e9b08970f9fe0002355b33733f09039131d7fc
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-11 05:07:20 -0800

    15-212 - setting up form, add simple spring backend

--- 15-vue-resource/210-vue-resource/src/App.vue
+++ 15-vue-resource/210-vue-resource/src/App.vue
@@ -4,12 +4,34 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
             </div>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" v-model="user.username">
+            </div>
+            <div class="form-group">
+                <label>Mail</label>
+                <input type="text" class="form-control" v-model="user.email">
+            </div>
+            <button class="btn btn-primary" @click="submit">Submit</button>
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        methods: {
+          submit() {
+              console.log(this.user);
+          }
+        },
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                }
+            }
+        }
     }
 </script>
 
---
+++ fifteen-backend/src/main/java/udemy/fifteenbackend/FifteenBackendApplication.java
@@ -0,0 +1,14 @@
+package udemy.fifteenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifteenBackendApplication.class, args);
+    }
+
+}
+
